21b22171ff1051aaf8404a230748a85287cfa41e
fabuss254 <fabuss254@outlook.fr>
2019-01-13 21:54:32 +0100

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -15,10 +15,8 @@ var Arr = ["http://dc-slf.glitch.me/","https://productive-violin.glitch.me/"]
 
 setInterval(function(){
   for(var i=0; Arr.length > i; i++){
-    console.log(Arr[i]);
-    setTimeout(function(){
       console.log("Requesting "+Arr[i]);
       ax(Arr[i]);
-    },(4000/Arr.length)*i);
+    //},(4000/Arr.length)*i);
   }
 }, 4000)
